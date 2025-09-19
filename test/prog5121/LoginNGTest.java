d4c46a2c5681bf430db2a554b69ad5aa5e9f99d4
RC_Student_lab <st10472587@rcconnect.edu.za>
2025-09-19 16:35:21 +0000

    Last commit

---
+++ test/prog5121/LoginNGTest.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package prog5121;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LoginNGTest {
+    
+    public LoginNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of checkUserName method, of class Login.
+     */
+    @Test
+    public void testCheckUserName() {
+        System.out.println("checkUserName");
+        String username = "";
+        Login instance = new Login();
+        boolean expResult = false;
+        boolean result = instance.checkUserName(username);
+        
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+        System.out.println("checkPasswordComplexity");
+        String password = "";
+        Login instance = new Login();
+        boolean expResult = false;
+        boolean result = instance.checkPasswordComplexity(password);
+        
+    }
+
+    /**
+     * Test of checkCellPhoneNumber method, of class Login.
+     */
+    @Test
+    public void testCheckCellPhoneNumber() {
+        System.out.println("checkCellPhoneNumber");
+        String phone = "";
+        Login instance = new Login();
+        boolean expResult = false;
+        boolean result = instance.checkCellPhoneNumber(phone);
+       
+    }
+
+    /**
+     * Test of registerUser method, of class Login.
+     */
+    @Test
+    public void testRegisterUser() {
+        System.out.println("registerUser");
+        String username = "";
+        String password = "";
+        String phone = "";
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.registerUser(username, password, phone);
+      
+    }
+
+    /**
+     * Test of returnLoginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnLoginStatus() {
+        System.out.println("returnLoginStatus");
+        boolean isSuccessful = false;
+        String firstName = "";
+        String lastName = "";
+        Login instance = new Login();
+        String expResult = "";
+        String result = instance.returnLoginStatus(isSuccessful, firstName, lastName);
+        
+    }
+    
+}
